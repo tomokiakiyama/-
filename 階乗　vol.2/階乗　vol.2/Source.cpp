f7906f2a3d062960f8d6bf2ea67c542694d82b56
秋山智暉 <tomokiakiyama@kxb.biglobe.ne.jp>
2016-06-13 15:38:04 +0900

    階乗vol.2

---
+++ 階乗　vol.2/階乗　vol.2/Source.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int num, num1, num2;
+	printf("値を入力してください。\n");//値を出力する。
+	scanf_s("%d", &num);//値を入力する。
+
+	if (num >= 1) {
+		for (num1 = num, num2 = num - 1; num2 >= 1; num1 = num1*num2, num2 = num2--) {}//階乗の計算を行なう。
+
+		printf("%d!は%dです。\n", num, num1);//階乗を出力する。
+	}
+	else {}
+
+	return 0;
+}
