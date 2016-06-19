2e59e0b7c60f7f512b1d01e07a55381beae3cbfa
秋山智暉 <tomokiakiyama@kxb.biglobe.ne.jp>
2016-06-19 15:36:18 +0900

    図形

--- 図形/図形/Source.cpp
+++ 図形/図形/Source.cpp
@@ -4,17 +4,17 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 1; a <= 5; a++) {
+	for (a = 1; a <= 5; a++) {//繰り返す。
 
-		for (c = 5; c >= 5 - (5 - a); c = c - 1) {
-			printf(" ");
+		for (c = 5; c >= 5 - (5 - a); c = c - 1) {//繰り返す
+			printf(" ");//空欄を出力する。
 		}
 
-		for (b = 1; b <= a * 2 - 1; b = b++) {
-			printf("*");
+		for (b = 1; b <= a * 2 - 1; b = b++) {//繰り返す
+			printf("*");//*を出力する。
 		}
 		
-		printf("\n");
+		printf("\n");//改行する。
 	}
 	
 	return 0;
