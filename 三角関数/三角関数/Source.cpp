639d38d949a9d464b8c906d8395090c901c0148c
秋山智暉 <tomokiakiyama@kxb.biglobe.ne.jp>
2016-06-24 00:39:26 +0900

    三角関数

---
+++ 三角関数/三角関数/Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int a;
+	double b,c;
+
+	printf("角度　  sin       cos        tan\n");//出力する。
+
+	for (a = 0, b = 0, c = 0; a <= 90; a++, c++) {//０度から９０度まで繰り返す。
+
+		b = c*3.14159265 / 180;
+
+		printf("%d    %lf   %lf   %lf\n", a, sin(b), cos(b), tan(b));//sin,cos,tanを出力する。
+	}
+	return 0;
+}
