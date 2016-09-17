770f61ab64b1e3cf2f9a73c125d32a4778560fd9
秋山智暉 <tomokiakiyama@kxb.biglobe.ne.jp>
2016-09-18 06:01:35 +0900

    整数定数

---
+++ 整数定数/整数定数/Source.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+	int a;
+
+	printf("’l‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+
+	scanf_s("%d", &a);
+	printf("‚P‚Oi”‚Í%d@‚Wi”‚Í%o ‚P‚Ui”‚Í%x\n", a, a, a);
+
+	return 0;
+
+}
